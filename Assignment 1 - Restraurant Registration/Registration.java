38548768ac7cbc941600733137f4e90ca6c9c95c
Enrique Romero <enrique.romero1110@gmail.com>
2023-05-23 19:08:11 -0400

    first commit

---
+++ Assignment 1 - Restraurant Registration/Registration.java
@@ -0,0 +1,9 @@
+public class Registration {
+    public static void main(String[] args) {
+        // Print required information using System out
+        System.out.println("First Name: Enrique");
+        System.out.println("Last Name: Romero");
+        System.out.println("Location: United States of America");
+        System.out.println("Employees: 101-1000");
+    }
+}
