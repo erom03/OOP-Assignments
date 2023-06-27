587b13d55336b3094d65144cc4acf2f704225b53
Enrique Romero <enrique.romero1110@gmail.com>
2023-06-27 14:43:10 -0400

    Add file

---
+++ Assignment 6 - Obnoxious Obstructions/ObnoxiousTemplate.java
@@ -0,0 +1,36 @@
+public class Obnoxious {
+    // Write the solution to the problem in Main using the Customer class
+    public static void main(String[] args) {
+        
+    }
+}
+
+// This class will store important information about the Customer
+class Customer {
+    // Stores the name of the customer
+    final String name;
+    // Stores the time the customer enters the line
+    final int enterTime;
+    // Stores the number of cats distracting this customer
+    private int numDistractions;
+
+    // Constructor to initialize name, enterTime, and numDistractions
+    Customer(String name, int enterTime) {
+        /* TODO */
+    }
+
+    // Add a distraction to the customer
+    public void addDistraction() {
+        /* TODO */
+    }
+
+    // Remove a distraction from the customer
+    public void removeDistraction() {
+        /* TODO */
+    }
+
+    // Check if the customer is distracted by any cats
+    public boolean isDistracted() {
+        /* TODO */
+    }
+}
