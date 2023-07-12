6f942a94b4736a98cf1b9becc20675856e797276
Enrique Romero <enrique.romero1110@gmail.com>
2023-07-12 19:26:35 -0400

    Did a lot, mainly changed the way my usefulness to employee map worked. Also finished the month functionality. Added some comments to employee class and changed what the compareTo method compared from usefulness to name.

--- Assignment 7 - Compliance Competition/Compliance.java
+++ Assignment 7 - Compliance Competition/Compliance.java
@@ -6,7 +6,7 @@ public class Compliance {
         Scanner sc = new Scanner(System.in);
 
         // Track employees and their usefulness
-        TreeMap<Employee, Long> employeeToUsefulness = new TreeMap<>();
+        TreeMap<Long, TreeSet<Employee>> usefulnessToEmployee = new TreeMap<>();
 
         // Track Names to which employee
         HashMap<String, Employee> nameToEmployee = new HashMap<>();
@@ -27,8 +27,21 @@ public class Compliance {
                 // Create new temp employee with needed info
                 Employee employee = new Employee(name, usefulness);
 
-                // Add info to Employee - Usefullness map
-                employeeToUsefulness.put(employee, usefulness);
+                // Add info to Usefullness- Employee map
+                // Check if key already exists
+                if(usefulnessToEmployee.containsKey(usefulness)) {
+                    // Add employee to existing set
+                    usefulnessToEmployee.get(usefulness).add(employee);
+                } else {
+                    // Create set
+                    TreeSet<Employee> temp = new TreeSet<>();
+
+                    // Add employee to set
+                    temp.add(employee);
+
+                    // Add values to map
+                    usefulnessToEmployee.put(usefulness, temp);
+                }
 
                 // Add info to Name - Employee map
                 nameToEmployee.put(name, employee);
@@ -42,18 +55,67 @@ public class Compliance {
                 
                 // Get employee
                 Employee employee = nameToEmployee.get(name);
-                
-                // Calculate new employee usefulness
-                Long newUsefulness = employee.getUsefulness() + adjustment;
 
-                // Replace the old employee with the new employee in the TreeMap
-                employeeToUsefulness.replace(employee, newUsefulness);
+                // Track old usefulness value
+                Long oldUsefulness = employee.getUsefulness();
+                
+                // Remove employee from Usefulness set
+                usefulnessToEmployee.get(oldUsefulness).remove(employee);
+
+                // Check if set is now empty
+                if(usefulnessToEmployee.get(oldUsefulness).isEmpty()) {
+                    // Remove entry from map
+                    usefulnessToEmployee.remove(oldUsefulness);
+                }
+
+                // Track new usefulness value and adjust the old one
+                Long newUsefulness = employee.adjustUsefulness(adjustment);
+
+                // Replace the employee in our name hashmap
+                nameToEmployee.replace(name, employee);
+
+                // Replace the employee in our usefulness map
+                // Check if key already exists
+                if(usefulnessToEmployee.containsKey(newUsefulness)) {
+                    // Add employee to existing set
+                    usefulnessToEmployee.get(newUsefulness).add(employee);
+                } else {
+                    // Create set
+                    TreeSet<Employee> temp = new TreeSet<>();
+
+                    // Add employee to set
+                    temp.add(employee);
+
+                    // Add values to map
+                    usefulnessToEmployee.put(newUsefulness, temp);
+                }
 
             } else if(input.startsWith("Month")) {  // Check for Month command
                 // Check if we even have an employee
                 if(nameToEmployee.isEmpty()) {
                     System.out.println("0");
                     continue;
+                }
+
+                // Get the top values from our Usefulness set
+                Long topUsefulness = usefulnessToEmployee.lastKey();
+
+                // Track the set of top Employees while removing them from the map
+                TreeSet<Employee> top = usefulnessToEmployee.remove(topUsefulness);
+
+                // Print how many employees are contained in the set (employees of the month)
+                System.out.println(top.size());
+
+                // iterate through the top employees
+                for(var currEmployee : top) {
+                    // Get the employees name
+                    String name = currEmployee.getName();
+
+                    // Print the name
+                    System.out.println(name);
+
+                    // Remove the entry from the name map
+                    nameToEmployee.remove(name);
                 }
             }
         }
@@ -74,20 +136,28 @@ class Employee implements Comparable<Employee> {
         this.usefulness = usefulness;
     }
 
+    // Returns the value of usefulness
     public Long getUsefulness() {
         return usefulness;
     }
 
+    // Returns employees name
     public String getName() {
         return name;
     }
 
+    // Used to compare names
     public int compareTo(Employee employee) {
-        return (employee.usefulness.compareTo(this.usefulness));
+        // The negative is a cursed way to sort things correctly
+        // dont judge
+        return -(employee.name.compareTo(this.name));
     }
 
-    public void adjustUsefulness(Long adjustment) {
+    // Allows for adjustment of usefulness
+    public Long adjustUsefulness(Long adjustment) {
         usefulness += adjustment;
+
+        return usefulness;
     }
 
 }
